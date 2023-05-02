2cb2de181c57bc7b3a3174f2bd62927dfb335967
Eric Elsner <ericelsnercodes@gmail.com>
2023-05-02 10:26:33 -0400

    initial commit, changed readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,50 @@
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `mrb-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Many Rivers Band Official Site`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+    plugins: [
+        'gatsby-plugin-google-gtag',
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-transformer-remark',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        {
+            resolve: 'gatsby-source-strapi',
+            options: {
+                apiURL: process.env.STRAPI_API_URL,
+                accessToken: process.env.STRAPI_TOKEN,
+                collectionTypes: [],
+                singleTypes: [],
+            },
+        },
+    ],
+}
